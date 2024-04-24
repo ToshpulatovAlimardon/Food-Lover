a05e00d812b5cb8b5ba2b9ea289e3b8541015d97
ToshpulatovAlimardon <alimardontoshpulatov001@gmail.com>
2024-04-24 11:16:43 +0500

    added showTabContent

--- js/script.js
+++ js/script.js
@@ -7,7 +7,17 @@ window.addEventListener("DOMContentLoaded", () => {
     tabContents.forEach(tabContent => {
       tabContent.style.display = 'none'
     })
+
+    tabs.forEach(tab => {
+      tab.classList.remove('tabheader__item_active')
+    })
+  }
+
+  function showTabContent(index) {
+    tabContents[index].style.display = 'flex'
+    tabs[index].classList.add('tabheader__item_active')
   }
 
   hideTabContents();
+  showTabContent(1);
 })
