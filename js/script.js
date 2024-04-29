873588d1620eab85a69f6d5e827e61ad205d75c4
ToshpulatovAlimardon <alimardontoshpulatov001@gmail.com>
2024-04-29 07:11:54 +0500

    changed hour number

--- js/script.js
+++ js/script.js
@@ -63,6 +63,14 @@ window.addEventListener("DOMContentLoaded", () => {
     };
   }
 
+  function formatNumber(number) {
+    if (number >= 0 && number < 10) {
+      return `0${number}`;
+    } else {
+      return number;
+    }
+  }
+
   function setClock(selector, endtime) {
     const timer = document.querySelector(selector),
       days = timer.querySelector("#days"),
@@ -71,13 +79,15 @@ window.addEventListener("DOMContentLoaded", () => {
       seconds = timer.querySelector("#seconds"),
       timeInterval = setInterval(updateClock, 1000);
 
+    updateClock();
+
     function updateClock() {
       const time = getTimeRemaining(endtime);
 
-      days.textContent = time.days;
-      hours.textContent = time.hours;
-      minutes.textContent = time.minutes;
-      seconds.textContent = time.seconds;
+      days.textContent = formatNumber(time.days);
+      hours.textContent = formatNumber(time.hours);
+      minutes.textContent = formatNumber(time.minutes);
+      seconds.textContent = formatNumber(time.seconds);
 
       if (time.totalTime <= 0) {
         clearInterval(timeInterval);
