1e46687cfa0e61c9cab21ca9096a3aacf0c6e76d
ToshpulatovAlimardon <alimardontoshpulatov001@gmail.com>
2024-05-07 14:15:11 +0500

    added use strict

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 window.addEventListener("DOMContentLoaded", () => {
   // Tabs
 
@@ -189,36 +191,6 @@ window.addEventListener("DOMContentLoaded", () => {
       this.parent.append(element);
     }
   }
-
-  // const offers = [
-  //   {
-  //     src: "./img/offer1.png",
-  //     alt: "Quattro Pasta",
-  //     title: "Quattro Pasta",
-  //     descr:
-  //       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quibusdam.",
-  //     discount: 55,
-  //     sale: 20,
-  //   },
-  //   {
-  //     src: "./img/offer2.png",
-  //     alt: "Vegertarian Pasta",
-  //     title: "Vegertarian Pasta",
-  //     descr:
-  //       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quibusdam.",
-  //     discount: 75,
-  //     sale: 25,
-  //   },
-  //   {
-  //     src: "./img/offer3.png",
-  //     alt: "Gluten-Free Pasta",
-  //     title: "Gluten-Free Pasta",
-  //     descr:
-  //       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quibusdam.",
-  //     discount: 25,
-  //     sale: 15,
-  //   },
-  // ];
 
   fetch("http://localhost:3000/offers", {
     method: "GET",
