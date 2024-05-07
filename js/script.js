4e547e0feddd5ba0f75d7c11ffb5bb3af86a685a
ToshpulatovAlimardon <alimardontoshpulatov001@gmail.com>
2024-05-07 13:59:58 +0500

    changed offer for json files

--- js/script.js
+++ js/script.js
@@ -190,48 +190,55 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  const offers = [
-    {
-      src: "./img/offer1.png",
-      alt: "Quattro Pasta",
-      title: "Quattro Pasta",
-      descr:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quibusdam.",
-      discount: 55,
-      sale: 20,
-    },
-    {
-      src: "./img/offer2.png",
-      alt: "Vegertarian Pasta",
-      title: "Vegertarian Pasta",
-      descr:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quibusdam.",
-      discount: 75,
-      sale: 25,
-    },
-    {
-      src: "./img/offer3.png",
-      alt: "Gluten-Free Pasta",
-      title: "Gluten-Free Pasta",
-      descr:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quibusdam.",
-      discount: 25,
-      sale: 15,
-    },
-  ];
+  // const offers = [
+  //   {
+  //     src: "./img/offer1.png",
+  //     alt: "Quattro Pasta",
+  //     title: "Quattro Pasta",
+  //     descr:
+  //       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quibusdam.",
+  //     discount: 55,
+  //     sale: 20,
+  //   },
+  //   {
+  //     src: "./img/offer2.png",
+  //     alt: "Vegertarian Pasta",
+  //     title: "Vegertarian Pasta",
+  //     descr:
+  //       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quibusdam.",
+  //     discount: 75,
+  //     sale: 25,
+  //   },
+  //   {
+  //     src: "./img/offer3.png",
+  //     alt: "Gluten-Free Pasta",
+  //     title: "Gluten-Free Pasta",
+  //     descr:
+  //       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quibusdam.",
+  //     discount: 25,
+  //     sale: 15,
+  //   },
+  // ];
 
-  offers.forEach((offer) => {
-    const { src, alt, descr, discount, sale, title } = offer;
-    new OfferMenu(
-      src,
-      alt,
-      title,
-      descr,
-      discount,
-      sale,
-      ".offers-items"
-    ).render();
-  });
+  fetch("http://localhost:3000/offers", {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((offer) => {
+        const { src, alt, descr, discount, sale, title } = offer;
+        new OfferMenu(
+          src,
+          alt,
+          title,
+          descr,
+          discount,
+          sale,
+          ".offers-items"
+        ).render();
+      });
+    });
 
   // FORM
 
